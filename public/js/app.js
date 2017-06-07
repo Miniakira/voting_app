1c3cce8eb59bc140b235e02eb5f78105f6adb66b
Megaakira <akiiira@gmail.com>
2017-06-08 00:24:15 +0200

    Upvotes working and it's sorting correctly

--- public/js/app.js
+++ public/js/app.js
@@ -1,49 +1,64 @@
+
 class ProductList extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      products: []
+      products: [],
     };
+
+    this.handleProductUpVote = this.handleProductUpVote.bind(this);
   }
 
   componentDidMount() {
-    this.setState({ products: Seed.products
+    this.setState({ products: Seed.products });
+  }
+
+  // Inside `ProductList`
+  handleProductUpVote(productId) {
+    const nextProducts = this.state.products.map((product) => {
+      if (product.id === productId) {
+        return Object.assign({}, product, {
+          votes: product.votes + 1,
+        });
+      } else {
+        return product;
+      }
+    });
+    this.setState({
+      products: nextProducts,
     });
   }
-  handleProductUpVote(productId) {
-    console.log(productId + ' was upvoted.');
-  }
 
-  render(){
-    const products = Seed.products.sort((a,b) =>(
+  render() {
+    const products = this.state.products.sort((a, b) => (
       b.votes - a.votes
     ));
-
-      const productComponents = Seed.products.map((product) => (
-        <Product
-          key={'product-' + product.id}
-          id={product.id}
-          title={product.title}
-          description={product.description}
-          url={product.url}
-          votes={product.votes}
-          submitterAvatarUrl={product.submitterAvatarUrl}
-          productImageUrl={product.productImageUrl}
-          onVote={this.handleProductUpVote}
-        />
+    const productComponents = products.map((product) => (
+      <Product
+        key={'product-' + product.id}
+        id={product.id}
+        title={product.title}
+        description={product.description}
+        url={product.url}
+        votes={product.votes}
+        submitterAvatarUrl={product.submitterAvatarUrl}
+        productImageUrl={product.productImageUrl}
+        onVote={this.handleProductUpVote}
+      />
     ));
-    return(
-      <div className="ui unstackable items">
+    return (
+      <div className='ui unstackable items'>
         {productComponents}
       </div>
-  )}
+    );
+  }
 }
 
 class Product extends React.Component {
   constructor(props) {
     super(props);
-    // custom method bindings here”
+
     this.handleUpVote = this.handleUpVote.bind(this);
   }
 
@@ -52,37 +67,38 @@ class Product extends React.Component {
   }
 
   render() {
-    return(
+    return (
       <div className='item'>
         <div className='image'>
           <img src={this.props.productImageUrl} />
         </div>
-      <div className='middle aligned content'>
-        <div className='header'>
-          <a onClick={this.handleUpVote}>
-            <i className='large caret up icon' />
-          </a>
-          {this.props.votes}
-        </div>
-        <div className='description'>
-          <a href={this.props.url}>
-            {this.props.title}
-          </a>
-        <p>
-          {this.props.description}
-        </p>
-        </div>
-        <div className='extra'>
-          <span>Submitted by:</span>
-        <img className='ui avatar image' src={this.props.submitterAvatarUrl} />
+        <div className='middle aligned content'>
+          <div className='header'>
+            <a onClick={this.handleUpVote}>
+              <i className='large caret up icon' />
+            </a>
+            {this.props.votes}
+          </div>
+          <div className='description'>
+            <a href={this.props.url}>
+              {this.props.title}
+            </a>
+            <p>
+              {this.props.description}
+            </p>
+          </div>
+          <div className='extra'>
+            <span>Submitted by:</span>
+            <img
+              className='ui avatar image'
+              src={this.props.submitterAvatarUrl}
+            />
+          </div>
         </div>
       </div>
-    </div>
-
-    )
+    );
   }
 }
-
 
 ReactDOM.render(
   <ProductList />,
